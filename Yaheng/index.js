06f9f2afa8bac0f779ee2b9bd0d98b3a0400b741
wuyaheng <wuyaheng2016@gmail.com>
2021-11-03 20:30:44 -0400

    get 100 random students' name, age and GPA

--- Yaheng/index.js
+++ Yaheng/index.js
@@ -1,1 +1,19 @@
-console.log("script tag");
+function getUsers() {
+    fetch("https://randomuser.me/api/?results=100")
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        fetchedResult = data.results;
+        console.log(fetchedResult)
+        fetchedResult.forEach((ele) => {
+            obj = {"name": ele.name.last + " " + ele.name.first,
+                    "age": ele.dob.age,
+                    "GPA": Math.floor(Math.random() * 4) + 1
+        }
+        console.log(obj)
+        })
+      });
+  }
+  
+getUsers();
