d91aa84b070b56a8f9d5287f86d4c8bb47293182
wuyaheng <wuyaheng2016@gmail.com>
2021-11-03 21:05:05 -0400

    push data to a list

--- Yaheng/index.js
+++ Yaheng/index.js
@@ -5,14 +5,16 @@ function getUsers() {
       })
       .then((data) => {
         fetchedResult = data.results;
-        console.log(fetchedResult)
+        studentData = []
         fetchedResult.forEach((ele) => {
             obj = {"name": ele.name.last + " " + ele.name.first,
                     "age": ele.dob.age,
                     "GPA": Math.floor(Math.random() * 4) + 1
         }
-        console.log(obj)
+        studentData.push(obj)
         })
+
+        console.log(studentData)
       });
   }
   
