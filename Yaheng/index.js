19211d7c9c90cc65a22f8bb9b3e45a946a96ddb3
Giupi De Luca <giupi.deluca@hotmail.com>
2021-11-04 09:09:04 -0400

    Yaheng push by Giuseppe, still work in progress

--- Yaheng/index.js
+++ Yaheng/index.js
@@ -1,21 +1,40 @@
 function getUsers() {
-    fetch("https://randomuser.me/api/?results=100")
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        fetchedResult = data.results;
-        studentData = []
-        fetchedResult.forEach((ele) => {
-            obj = {"name": ele.name.last + " " + ele.name.first,
-                    "age": ele.dob.age,
-                    "GPA": Math.floor(Math.random() * 4) + 1
-        }
-        studentData.push(obj)
-        })
-
-        console.log(studentData)
+  fetch("https://randomuser.me/api/?results=100")
+    .then((results) => {
+      return results.json();
+    })
+    .then((data) => {
+      fetchedResult = data.results;
+      studentData = [];
+      fetchedResult.forEach((ele) => {
+        obj = {
+          name: ele.name.last + " " + ele.name.first,
+          age: ele.dob.age,
+          GPA: Math.floor(Math.random() * 4) + 1,
+        };
+        studentData.push(obj);
       });
-  }
-  
+      console.log(studentData);
+      displayStudentTable(studentData);
+    });
+}
+
 getUsers();
+
+const studentTable = document.getElementById("studentTable");
+
+function displayStudentTable(studentData) {
+  studentData.forEach((student) => {
+    displayOneStudent(student);
+  });
+}
+
+function displayOneStudent(student) {
+  
+  let html = `<div class="row">
+  <span>Name: ${student.name}</span>
+  <span>Age: ${student.age}</span>
+  <span>GPA: ${student.GPA}</span>
+  </div>`;
+  studentTable.innerHTML += html;
+}
